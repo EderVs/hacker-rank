17618daddced16a58553d4725e9fd168b9ffb9c6
EderVs <eedervs.97@gmail.com>
2015-10-10 23:29:19 -0500

    Area Under Curves and Volume of Revolving a Curve completed. YEAH!

--- functional_programming/introduction/area_under_curves_and_volume_of_revolving_a_curve/area_under_curves_and_volume_of_revolving_a_curve.hs
+++ functional_programming/introduction/area_under_curves_and_volume_of_revolving_a_curve/area_under_curves_and_volume_of_revolving_a_curve.hs
@@ -2,16 +2,19 @@ import Text.Printf (printf)
 
 -- This function should return a list [area, volume].
 solve :: Int -> Int -> [Int] -> [Int] -> [Double]
-solve l r a b = [g (l * 1000) (r * 1000) 1 a b, 0.0]
+solve l r a b = [getArea (l * 1000) (r * 1000) 1 a b, getVolume (l * 1000) (r * 1000) 1 a b]
 
 equation :: [Int] -> [Int] -> Int -> Double
 equation a b x = sum [(fromIntegral ((a)!!i))*((fromIntegral x)**((fromIntegral (b!!i))))/1000^^(b!!i) | i <- [0..(length a) - 1]]
 
-f :: [Int] -> Int -> Int -> [Int]
-f lst x limit = if ((head lst)) < limit - x then f (((head lst) + x):lst) x limit else lst
+getSides :: [Int] -> Int -> Int -> [Int]
+getSides lst x limit = if ((head lst)) < limit - x then getSides (((head lst) + x):lst) x limit else lst
 
-g :: Int -> Int -> Int -> [Int] -> [Int] -> Double
-g l r x a b = sum [(equation a b z)*((fromIntegral x)/1000) | z <- ((f [l] x r))]
+getArea :: Int -> Int -> Int -> [Int] -> [Int] -> Double
+getArea l r x a b = sum [(equation a b z)*((fromIntegral x)/1000) | z <- ((getSides [l] x r))]
+
+getVolume :: Int -> Int -> Int -> [Int] -> [Int] -> Double
+getVolume l r x a b = pi * (sum [(((equation a b z)*((fromIntegral x)/1000))**2.0) * 1000 | z <- ((getSides [l] x r))])
 
 --Input/Output.
 main :: IO ()
